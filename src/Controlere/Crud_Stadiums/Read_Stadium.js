3e870849b21f8218178c0e8c69140f0d63875052
yanastany <anastan0205@gmail.com>
2022-12-01 17:28:54 +0200

    test2

--- src/Controlere/Crud_Stadiums/Read_Stadium.js
+++ src/Controlere/Crud_Stadiums/Read_Stadium.js
@@ -5,81 +5,122 @@ import {app} from '../../DatabaseConnection';
 import React,{useState,useEffect} from 'react';
 const db = getFirestore(app);
 
+const useSortableData = (items, config = null) => {
+    const [sortConfig, setSortConfig] = React.useState(config);
+  
+    const sortedItems = React.useMemo(() => {
+      let sortableItems = [...items];
+      if (sortConfig !== null) {
+        sortableItems.sort((a, b) => {
+          if (a[sortConfig.key] < b[sortConfig.key]) {
+            return sortConfig.direction === 'ascending' ? -1 : 1;
+          }
+          if (a[sortConfig.key] > b[sortConfig.key]) {
+            return sortConfig.direction === 'ascending' ? 1 : -1;
+          }
+          return 0;
+        });
+      }
+      return sortableItems;
+    }, [items, sortConfig]);
+  
+    const requestSort = (key) => {
+      let direction = 'ascending';
+      if (
+        sortConfig &&
+        sortConfig.key === key &&
+        sortConfig.direction === 'ascending'
+      ) {
+        direction = 'descending';
+      }
+      setSortConfig({ key, direction });
+    };
+    //console.log(sortedItems)
+    return { items: sortedItems, requestSort, sortConfig };
+  };
+
+
 function Read_Stadiums(){
   const [stadioane, setStadioane] = useState([]);
+  
   const fetchStadioane = async()=>{
     let response=collection(db, 'stadion');
-    let data =await getDocs(response);
-    
-    data.docs.forEach(item=>{
-      setStadioane([...stadioane, item.data()])
-    })
+    let data =await getDocs(response).then((querySnapshot) => {
+
+      querySnapshot.forEach(element => {
+          var date = element.data();
+          setStadioane(arr => [...arr , date]);
+
+      });
+  });
+
   }
+
     useEffect(()=>{
       fetchStadioane();
     },[])
-    console.log(stadioane);
+    
+    const { items, requestSort, sortConfig } = useSortableData(stadioane);
+    const getClassNamesFor = (name) => {
+        if (!sortConfig) {
+            return;
+        }
+        return sortConfig.key === name ? sortConfig.direction : undefined;
+        };
+
+console.log(stadioane)
     return(
         
-    //   <div className="App">
-    //   {
-    //     stadioane && stadioane.map(blog=>{
-    //       return(
-    //         <div className="stadioane-container">
-    //           <h4>{blog.adresa}</h4>
-    //           <p>{blog.capacitate}</p>
-    //           <p>{blog.denumire}</p>
-    //           <p>{blog.tip_gazon}</p>
-    //         </div>
-    //       )
-    //     })
-    //   }
-    // </div>
-
-        
-    <Table singleLine className='tabel'>
-                <Table.Header className='tt1'>
-                    <Table.Row>
-                    
-                        <Table.HeaderCell className='titlu'><button
-              type="button"
+   
+        <Table singleLine className='tabel'>
+        <Table.Header className='tt1'>
+            <Table.Row>
             
-            >Name</button></Table.HeaderCell>
-                        <Table.HeaderCell className='titlu'><button
-              type="button"
             
-            >Capacity</button></Table.HeaderCell>
-                        <Table.HeaderCell className='titlu'><button
-              type="button"
-              
-            >Surface</button></Table.HeaderCell>
-                        <Table.HeaderCell className='titlu'><button
-              type="button"
-           
-            >Address</button></Table.HeaderCell>
-                        
-                        
-
-                    </Table.Row>
-                </Table.Header>
-
-                <Table.Body>
+                <Table.HeaderCell className='titlu'><button
+      type="button"
+      onClick={() => requestSort('name')}
+      className={getClassNamesFor('name')}
+    >Name</button></Table.HeaderCell>
+                <Table.HeaderCell className='titlu'><button
+      type="button"
+      onClick={() => requestSort('capacity')}
+      className={getClassNamesFor('capacity')}
+    >Capacity</button></Table.HeaderCell>
+                <Table.HeaderCell className='titlu'><button
+      type="button"
+      onClick={() => requestSort('surface')}
+      className={getClassNamesFor('surface')}
+    >Surface</button></Table.HeaderCell>
+                <Table.HeaderCell className='titlu'><button
+      type="button"
+      onClick={() => requestSort('address')}
+      className={getClassNamesFor('address')}
+    >Address</button></Table.HeaderCell>
                 
-                {stadioane && stadioane.map((data) => {
-     return (
-       <Table.Row>
+                
+
+            </Table.Row>
+        </Table.Header>
+
+        <Table.Body>
         
-          <Table.Cell >{data.denumire}</Table.Cell>
+        {stadioane && stadioane.map((data) =>  {
+return (
+<Table.Row key = {data.denumire}>
+
+<Table.Cell >{data.denumire}</Table.Cell>
           <Table.Cell >{data.capacitate}</Table.Cell>
           <Table.Cell >{data.tip_gazon}</Table.Cell>
           <Table.Cell >{data.adresa}</Table.Cell>
-          
-          
+  
+ 
 
-        </Table.Row>
-   )})}
-                </Table.Body>
-            </Table>
+</Table.Row>
+)})}
+        </Table.Body>
+    </Table>
+   
     );
 }
 
